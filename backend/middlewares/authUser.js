4e5664f9bd568c6c6c4b0e726323411d146bca14
Adarsh Sugandhe <adarshsugandhe2024@gmail.com>
2025-09-12 17:15:34 +0530

    added client url

--- backend/middlewares/authUser.js
+++ backend/middlewares/authUser.js
@@ -3,9 +3,9 @@ import jwt from 'jsonwebtoken'
 // user authenticaiton middleware
 const authUser = async (req, res, next) => {
   try {
-    const {token} = req.headers
+    const { token } = req.headers
     if (!token) {
-      return res.json({success: false, message: "Not Authorized Login Again"})
+      return res.json({ success: false, message: "Not Authorized Login Again" })
     }
 
     const token_decode = jwt.verify(token, process.env.JWT_SECRET)
@@ -13,10 +13,10 @@ const authUser = async (req, res, next) => {
     req.userId = token_decode.id
 
     next()
-    
+
   } catch (error) {
     console.log(error)
-    res.json({success: false, message:error.message})
+    res.json({ success: false, message: error.message })
   }
 }
 
